60af3c1ac5b931a2bba34190bb693469004b440c
tsubasanoyume <tsubasaro@gmail.com>
2016-10-13 13:27:09 +0300

    Add files via upload

---
+++ nerds/scripts/script.js
@@ -0,0 +1,40 @@
+/**
+ * Created by e.lebedeva on 12.10.2016.
+ */
+var slide1 = document.querySelector('.slide-1'),
+    slide2 = document.querySelector('.slide-2'),
+    slide3 = document.querySelector('.slide-3'),
+    checkbox1 = document.querySelector('#radio-1'),
+    checkbox2 = document.querySelector('#radio-2'),
+    checkbox3 = document.querySelector('#radio-3'),
+    formBlock = document.querySelector('.contact-us'),
+    btnContactUs = document.querySelector('.btn-contacts'),
+    btnClose = document.querySelector('.btn-close');
+
+checkbox1.addEventListener('change', function () {
+    slide2.classList.remove('active-slide');
+    slide3.classList.remove('active-slide');
+    slide1.classList.add('active-slide');
+});
+
+checkbox2.addEventListener('change', function () {
+    slide1.classList.remove('active-slide');
+    slide3.classList.remove('active-slide');
+    slide2.classList.add('active-slide');
+});
+
+checkbox3.addEventListener('change', function () {
+    slide1.classList.remove('active-slide');
+    slide2.classList.remove('active-slide');
+    slide3.classList.add('active-slide');
+});
+
+btnContactUs.addEventListener('click', function (e) {
+    e.preventDefault();
+    formBlock.classList.toggle('contact-us-show');
+});
+
+btnClose.addEventListener('click', function (e) {
+    e.preventDefault();
+    formBlock.classList.remove('contact-us-show');
+});
