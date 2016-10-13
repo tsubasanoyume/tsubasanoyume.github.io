56e9367e362245b70d6c41e2e22a38f84ff1533d
tsubasanoyume <tsubasaro@gmail.com>
2016-10-13 17:36:25 +0300

    Add files via upload

--- nerds/scripts/script.js
+++ nerds/scripts/script.js
@@ -1,33 +1,37 @@
 /**
  * Created by e.lebedeva on 12.10.2016.
  */
-var slide1 = document.querySelector('.slide-1'),
-    slide2 = document.querySelector('.slide-2'),
-    slide3 = document.querySelector('.slide-3'),
-    checkbox1 = document.querySelector('#radio-1'),
-    checkbox2 = document.querySelector('#radio-2'),
-    checkbox3 = document.querySelector('#radio-3'),
-    formBlock = document.querySelector('.contact-us'),
+if (document.title == 'Nerds') {
+    var slide1 = document.querySelector('.slide-1'),
+        slide2 = document.querySelector('.slide-2'),
+        slide3 = document.querySelector('.slide-3'),
+        checkbox1 = document.querySelector('#radio-1'),
+        checkbox2 = document.querySelector('#radio-2'),
+        checkbox3 = document.querySelector('#radio-3');
+
+    checkbox1.addEventListener('change', function () {
+        slide2.classList.remove('active-slide');
+        slide3.classList.remove('active-slide');
+        slide1.classList.add('active-slide');
+    });
+
+    checkbox2.addEventListener('change', function () {
+        slide1.classList.remove('active-slide');
+        slide3.classList.remove('active-slide');
+        slide2.classList.add('active-slide');
+    });
+
+    checkbox3.addEventListener('change', function () {
+        slide1.classList.remove('active-slide');
+        slide2.classList.remove('active-slide');
+        slide3.classList.add('active-slide');
+    });
+}
+
+var formBlock = document.querySelector('.contact-us'),
     btnContactUs = document.querySelector('.btn-contacts'),
     btnClose = document.querySelector('.btn-close');
 
-checkbox1.addEventListener('change', function () {
-    slide2.classList.remove('active-slide');
-    slide3.classList.remove('active-slide');
-    slide1.classList.add('active-slide');
-});
-
-checkbox2.addEventListener('change', function () {
-    slide1.classList.remove('active-slide');
-    slide3.classList.remove('active-slide');
-    slide2.classList.add('active-slide');
-});
-
-checkbox3.addEventListener('change', function () {
-    slide1.classList.remove('active-slide');
-    slide2.classList.remove('active-slide');
-    slide3.classList.add('active-slide');
-});
 
 btnContactUs.addEventListener('click', function (e) {
     e.preventDefault();
